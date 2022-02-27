a0b3ab9e795a10fc7d0b1770c8077b3a92aae320
teknikal <teknikal@birutekno.com>
2022-02-27 15:56:33 +0700

    frontend view has been created

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <WomenHero />
+    <Instagram />
+    <Partner />
+    <Footer />
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue';
+import Hero from '@/components/Hero.vue';
+import WomenHero from '@/components/WomenHero.vue';
+import Instagram from '@/components/Instagram.vue';
+import Partner from '@/components/Partner.vue';
+import Footer from '@/components/Footer.vue';
+
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    WomenHero,
+    Instagram,
+    Partner,
+    Footer
+  }
+}
+</script>
